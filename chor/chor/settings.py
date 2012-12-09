da4f6fd3ecc1f647686ed58c30ae33366efaa9f5
Oliver Schmidt <spiollinux@googlemail.com>
2012-12-09 01:15:24 +0100

    fixing static files from bootstrap

--- chor/chor/settings.py
+++ chor/chor/settings.py
@@ -85,6 +85,7 @@ STATICFILES_DIRS = (
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
     rel('staticfiles'),
+    ('bootstrap',rel('../submodules/twitterbootstrap/docs/assets/')),
 )
 
 # List of finder classes that know how to find static files in
